4721b5a599a1d57628debb6c15773fd445ba1a37
Srisaiyeegharan <srisaiyeegharan.kidnapillai@team.telstra.com>
2018-03-14 13:28:51 +1100

    Initial Commit

---
+++ src/main/java/com/example/SpringBootSample/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.example.SpringBootSample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api-sample/")
+public class ApplicationController {
+	
+	@RequestMapping("/hi")
+	private String getHi() {
+		return "Hi Sri - App User is working on Openshift";
+	}
+	
+
+}
